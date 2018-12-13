28e976e8b8d2001cea914619261758548c8b65df
ihr@edilion.com <hrynchyshynKI33>
2018-12-13 11:45:13 +0200

    Added gitignore. Added annotation to AUser entity.

--- src/main/java/com/hrnchshn/finance/models/AUser.java
+++ src/main/java/com/hrnchshn/finance/models/AUser.java
@@ -1,8 +1,18 @@
 package com.hrnchshn.finance.models;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-@
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class AUser {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
